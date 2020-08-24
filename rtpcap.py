bea700630b4b891431665561da92187e419fe7ae
Chema Gonzalez <chemag@fb.com>
2020-08-24 13:46:06 -0700

    rtpcap: add 2x audio analysis
    Test Plan: Run both analysis in a pcap file.

--- rtpcap.py
+++ rtpcap.py
@@ -12,6 +12,8 @@ import sys
 default_values = {
     'debug': 0,
     'dry_run': False,
+    'analysis_type': 'all',
+    'filter': None,
     'infile': None,
 }
 
@@ -19,6 +21,13 @@ default_values = {
 IPV4_PATTERN = r'\d+\.\d+\.\d+\.\d+'
 IPV6_PATTERN = r'[a-fA-F\d:]+'
 IP_PATTERN = r'[a-fA-F\d:\.]+'
+
+ANALYSIS_TYPES = {
+    'video',
+    'audio-jitter',
+    'audio-ploss',
+    'all',
+}
 
 
 def run(command, options, **kwargs):
@@ -50,19 +59,21 @@ def run(command, options, **kwargs):
 def parse_file(infile, options):
     print('options: %r' % options)
     # get heavy hitters
-    output = analyze_udp_connections(infile, options)
-    if not output:
+    udp_connections = analyze_udp_connections(infile, options)
+    if not udp_connections:
         print('error: no UDP connections in %s' % infile)
         sys.exit(-1)
     if options.debug > 2:
-        for d in output:
+        for d in udp_connections:
             print(d)
-    conn = output[0]
+    conn = udp_connections[0]
     if options.debug > 0:
-        print('conn is {laddr}:{lport} <-> {raddr}:{rport} proto: {proto} bytes: {tbytes}'.format(**conn))
+        print('main_conn {{ left: {laddr}:{lport} right: {raddr}:{rport} '
+              'proto: {proto} bytes: {tbytes} packets: {tpkts} }}'.format(
+                  **conn))
     # process connection
     prefix = '%s.%s' % (infile, 'conn')
-    process_connection(conn, infile, prefix, options)
+    process_connection(infile, udp_connections, conn, prefix, options)
 
 
 # get heavy hitters
@@ -85,8 +96,8 @@ def get_addr_proto(addr):
 
 
 def parse_udp_connections(out, options):
-    output = []
-    # example: '192.168.1.32:5353          <-> 224.0.0.251:5353                 0         0       8      4276       8      4276     0.560657000         7.6643'
+    udp_connections = []
+    # example: '1.1.1.1:1111 <-> 2.2.2.2:2222 0 0 8 4276 8 4276 0.56065 7.6643'
     conn_pattern = (
         r'(?P<laddr>' + IP_PATTERN + r'):(?P<lport>\d*) *'
         r' <-> '
@@ -113,44 +124,101 @@ def parse_udp_connections(out, options):
             sys.exit(-1)
         d = {'proto': proto}
         d.update(match.groupdict())
-        output.append(d)
-    return output
+        udp_connections.append(d)
+    return udp_connections
 
 
 # process a single connection
-def process_connection(conn, infile, prefix, options):
-    # extract full connection
-    tshark_filter = '{proto}.addr=={laddr} && udp.port=={lport} && {proto}.addr=={raddr} && udp.port=={rport}'.format(**conn)
-    conn_file = '%s.pcap' % prefix
-    command = 'tshark -r %s -Y "%s" -w %s' % (infile, tshark_filter, conn_file)
-    returncode, out, err = run(command, options)
-    if returncode != 0:
-        print('Cannot run "%s": "%s"' % (command, err))
-        sys.exit(-1)
-    ## extract left connection
-    #ltshark_filter = '{proto}.src=={laddr} && udp.srcport=={lport} && {proto}.dst=={raddr} && udp.dstport=={rport}'.format(**conn)
-    #lconn_file = '%s.src_%s.pcap' % (prefix, conn['laddr'])
-    #command = 'tshark -r %s -Y "%s" -w %s' % (infile, ltshark_filter, lconn_file)
-    #returncode, out, err = run(command, options)
-    #if returncode != 0:
-    #    print('Cannot run "%s": "%s"' % (command, err))
-    #    sys.exit(-1)
-    ## extract right connection
-    #rtshark_filter = '{proto}.dst=={laddr} && udp.dstport=={lport} && {proto}.src=={raddr} && udp.srcport=={rport}'.format(**conn)
-    #rconn_file = '%s.src_%s.pcap' % (prefix, conn['raddr'])
-    #command = 'tshark -r %s -Y "%s" -w %s' % (infile, rtshark_filter, rconn_file)
-    #returncode, out, err = run(command, options)
-    #if returncode != 0:
-    #    print('Cannot run "%s": "%s"' % (command, err))
-    #    sys.exit(-1)
+def process_connection(infile, udp_connections, conn, prefix, options):
+    # create filter for full connection
+    conn_filter = '{proto}.addr=={laddr} && udp.port=={lport} && {proto}.addr=={raddr} && udp.port=={rport}'.format(**conn)
+    if options.filter is not None:
+        conn_filter += ' && ' + options.filter
 
-    # analyze connections
-    lvideo_statistics = analyze_rtp_data(conn_file, conn['laddr'],
-                                         conn['lport'], conn['proto'], options)
-    dump_video_statistics(lvideo_statistics, conn['laddr'], conn_file)
-    rvideo_statistics = analyze_rtp_data(conn_file, conn['raddr'],
-                                         conn['rport'], conn['proto'], options)
-    dump_video_statistics(rvideo_statistics, conn['raddr'], conn_file)
+    parsed_rtp_list = analyze_rtp_data(infile, conn_filter, conn['lport'],
+                                       conn['proto'], options)
+    # filter connection data
+
+    if options.debug > 0:
+        for ip_src in parsed_rtp_list.keys():
+            for rtp_ssrc in parsed_rtp_list[ip_src].keys():
+                for rtp_p_type in parsed_rtp_list[ip_src][rtp_ssrc].keys():
+                    ip_len = sum(d['ip_len'] for d in
+                                 parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type])
+                    pkts = len(parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type])
+                    print('ip_src: %s rtp_ssrc: %s rtp_p_type: %i '
+                          'ip_len: %i pkts: %i' % (
+                              ip_src, rtp_ssrc, rtp_p_type, ip_len, pkts))
+
+    ## analyze connections
+    #if options.analysis_type == 'video':
+    #    rtp_pkt_list = analyze_rtp_data(conn_file, conn['laddr'],
+    #                                    conn['lport'], conn['proto'], options)
+    #    # process RTP traffic
+    #    # get the video rtp_p_type
+    #    p_type_dict = classify_rtp_payload_types(rtp_pkt_list)
+    #    video_rtp_p_type = get_video_rtp_p_type(p_type_dict, saddr, options)
+    #    # parse video stream
+    #    lvideo_statistics = analyze_video_stream(rtp_pkt_list, video_rtp_p_type,
+    #                                             options)
+    #    dump_video_statistics(lvideo_statistics, conn['laddr'], conn_file)
+
+    #    rtp_pkt_list = analyze_rtp_data(conn_file, conn['raddr'],
+    #                                    conn['rport'], conn['proto'], options)
+    #    # process RTP traffic
+    #    # get the video rtp_p_type
+    #    p_type_dict = classify_rtp_payload_types(rtp_pkt_list)
+    #    video_rtp_p_type = get_video_rtp_p_type(p_type_dict, saddr, options)
+
+    #    # parse video stream
+    #    rvideo_statistics = analyze_video_stream(rtp_pkt_list, video_rtp_p_type,
+    #                                             options)
+    #    dump_video_statistics(rvideo_statistics, conn['raddr'], conn_file)
+
+    for ip_src in parsed_rtp_list.keys():
+        for rtp_ssrc in parsed_rtp_list[ip_src].keys():
+            for rtp_p_type in parsed_rtp_list[ip_src][rtp_ssrc].keys():
+                if options.analysis_type == 'audio-jitter':
+                    analyze_audio_jitter(infile, parsed_rtp_list, ip_src,
+                                         rtp_ssrc, rtp_p_type, options)
+                elif options.analysis_type == 'audio-ploss':
+                    analyze_audio_ploss(infile, parsed_rtp_list, ip_src,
+                                        rtp_ssrc, rtp_p_type, options)
+
+
+def analyze_audio_jitter(prefix, parsed_rtp_list, ip_src, rtp_ssrc, rtp_p_type,
+                         options):
+    output_file = '%s.audio.jitter.ip_src_%s.rtp_ssrc_%s.rtp_p_type_%i.csv' % (
+        prefix, ip_src, rtp_ssrc, rtp_p_type)
+    with open(output_file, 'w') as f:
+        delta_list = []
+        last_frame_time_relative = None
+        for pkt in parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type]:
+            if last_frame_time_relative is not None:
+                delta = pkt['frame_time_relative'] - last_frame_time_relative
+                delta_list.append([pkt['frame_time_relative'], delta])
+            last_frame_time_relative = pkt['frame_time_relative']
+        total_delta = sum(delta for _, delta in delta_list)
+        samples = len(delta_list)
+        average_delta = total_delta / samples
+        for frame_time_relative, delta in delta_list:
+            f.write('%f,%f,%f\n' % (frame_time_relative, delta, average_delta))
+
+
+def analyze_audio_ploss(prefix, parsed_rtp_list, ip_src, rtp_ssrc, rtp_p_type,
+                         options):
+    output_file = '%s.audio.ploss.ip_src_%s.rtp_ssrc_%s.rtp_p_type_%i.csv' % (
+        prefix, ip_src, rtp_ssrc, rtp_p_type)
+    with open(output_file, 'w') as f:
+        delta_list = []
+        last_rtp_seq = -1
+        for pkt in parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type]:
+            if last_rtp_seq != -1:
+                delta = pkt['rtp_seq'] - last_rtp_seq
+                delta_list.append([pkt['frame_time_relative'], delta])
+            last_rtp_seq = pkt['rtp_seq']
+        for frame_time_relative, delta in delta_list:
+            f.write('%f,%i\n' % (frame_time_relative, delta))
 
 
 def get_video_rtp_p_type(p_type_dict, saddr, options):
@@ -166,48 +234,42 @@ def get_video_rtp_p_type(p_type_dict, saddr, options):
     return video_rtp_p_type
 
 
-def analyze_rtp_data(infile, saddr, sport, proto, options):
-    ipsrc_field = '%s.src' % proto
-    iplen_field = 'ip.len' if proto == 'ip' else 'ipv6.plen'
-    command = ('tshark -r %s -d udp.port==%s,rtp '
-               '-n -T fields -e frame.number -e frame.time_epoch '
+def analyze_rtp_data(infile, conn_filter, sport, proto, options):
+    ip_src_field = '%s.src' % proto
+    ip_len_field = 'ip.len' if proto == 'ip' else 'ipv6.plen'
+    command = ('tshark -r %s '
+               '-d udp.port==%s,rtp '
+               '-Y "%s" '
+               '-n -T fields -e frame.number '
+               '-e frame.time_epoch -e frame.time_relative '
                '-e %s -e %s '
                '-e rtp.p_type -e rtcp.pt -e rtp.ssrc -e rtp.seq '
                '-e rtp.timestamp -e rtp.marker' % (
-                   infile, sport, ipsrc_field, iplen_field))
+                   infile, sport, conn_filter, ip_src_field, ip_len_field))
     returncode, out, err = run(command, options)
     if returncode != 0:
         print('Cannot run "%s": "%s"' % (command, err))
         sys.exit(-1)
-    parsed_pkt_list = parse_rtp_data(out, saddr, options)
-    rtp_pkt_list = parsed_pkt_list['rtp']
-    # rtcp_pkt_list = parsed_pkt_list['rtcp']
-    # process RTP traffic
-    # get the video rtp_p_type
-    p_type_dict = classify_rtp_payload_types(rtp_pkt_list)
-    video_rtp_p_type = get_video_rtp_p_type(p_type_dict, saddr, options)
-
-    # parse video stream
-    video_statistics = analyze_video_stream(rtp_pkt_list, video_rtp_p_type,
-                                            options)
-    return video_statistics
+    parsed_rtp_list, _ = parse_rtp_data(out, options)
+    return parsed_rtp_list
 
 
 def dump_video_statistics(video_statistics, saddr, conn_file):
     outfile = '%s.src_%s.video.csv' % (conn_file, saddr)
     with open(outfile, 'w') as f:
-        f.write('# frame_time_epoch, sec_pkts, sec_bits, sec_frames, '
+        f.write('# frame_time_epoch, frame_time_relative, '
+                'sec_pkts, sec_bits, sec_frames, '
                 'sec_max_frame_pkts, sec_max_frame_bits, sec_rtp_seq_issues, '
                 'sec_frame_pkts\n')
-        for (frame_time_epoch, sec_pkts, sec_bits, sec_frames,
+        for (frame_time_epoch, frame_time_relative,
+             sec_pkts, sec_bits, sec_frames,
              sec_max_frame_pkts, sec_max_frame_bits, sec_rtp_seq_issues,
              sec_frame_pkts) in video_statistics:
-            f.write('%i,%i,%i,%i,%i,%i,%i,%s\n' % (frame_time_epoch, sec_pkts,
-                                                   sec_bits, sec_frames,
-                                                   sec_max_frame_pkts,
-                                                   sec_max_frame_bits,
-                                                   sec_rtp_seq_issues,
-                                                   sec_frame_pkts))
+            f.write('%f.%f,%i,%i,%i,%i,%i,%i,%s\n' % (
+                frame_time_epoch, frame_time_relative,
+                sec_pkts, sec_bits, sec_frames,
+                sec_max_frame_pkts, sec_max_frame_bits,
+                sec_rtp_seq_issues, sec_frame_pkts))
     return 0
 
 
@@ -268,8 +330,8 @@ def analyze_video_stream(rtp_pkt_list, video_rtp_p_type, options):
         # account for the packet
         sec_pkts += 1
         frame_pkts += 1
-        frame_bytes += rtp_pkt['iplen']
-        sec_bytes += rtp_pkt['iplen']
+        frame_bytes += rtp_pkt['ip_len']
+        sec_bytes += rtp_pkt['ip_len']
         # check for end of frame
         if rtp_pkt['rtp_marker'] == 1:
             # account for the frame
@@ -298,28 +360,27 @@ def classify_rtp_payload_types(rtp_pkt_list):
             p_type_dict[rtp_p_type] = 0
         if rtp_p_type not in num_markers:
             num_markers[rtp_p_type] = 0
-        iplen = rtp_pkt['iplen']
-        p_type_dict[rtp_p_type] += iplen
+        ip_len = rtp_pkt['ip_len']
+        p_type_dict[rtp_p_type] += ip_len
         num_markers[rtp_p_type] += rtp_pkt['rtp_marker']
     for rtp_p_type in p_type_dict:
-        total_iplen = p_type_dict[rtp_p_type]
-        p_type_dict[rtp_p_type] = [total_iplen * 8. / duration_sec,
+        total_ip_len = p_type_dict[rtp_p_type]
+        p_type_dict[rtp_p_type] = [total_ip_len * 8. / duration_sec,
                                    num_markers[rtp_p_type]]
     return p_type_dict
 
 
-def parse_rtp_data(out, saddr, options):
-    output = {
-        'rtp': [],
-        'rtcp': [],
-    }
-    # example (rtp): '2\t1584723835.328870000\t2a03:2880:f231:cd:face:b00c:0:6443\t1135\t98\t\t0xd7346929\t27012\t1122654371\t0'
-    # example (rtcp): '3\t1584373728.001695000\t2601:647:4300:f039:e97a:e051:b8a8:a4da\t\t205'
+def parse_rtp_data(out, options):
+    parsed_rtp_list = {}
+    parsed_rtcp_list = {}
+    # example (rtp): '2\t1584723835.328870000\t0.0\t2a03:2880:f231:cd:face:b00c:0:6443\t1135\t98\t\t0xd7346929\t27012\t1122654371\t0'
+    # example (rtcp): '3\t1584373728.001695000\t0.00001\t2601:647:4300:f039:e97a:e051:b8a8:a4da\t\t205'
     pkt_pattern = (
         r'(?P<frame_number>\d+)\t'
         r'(?P<frame_time_epoch>[\d\.]+)\t'
-        r'(?P<ipsrc>' + IP_PATTERN + r')\t'
-        r'(?P<iplen>\d+)\t'
+        r'(?P<frame_time_relative>[\d\.]+)\t'
+        r'(?P<ip_src>' + IP_PATTERN + r')\t'
+        r'(?P<ip_len>\d+)\t'
         r'(?P<rtp_p_type>\d*)\t'  # optional
         r'(?P<rtcp_pt>\d*)\t*'  # optional
         r'(?P<rtp_ssrc>0x[\da-fA-F]*)\t*'  # optional
@@ -335,23 +396,32 @@ def parse_rtp_data(out, saddr, options):
             if options.debug > 2:
                 print('warning: invalid RTP line: "%s"' % line)
             continue
-        # filter the packet
-        if match.group('ipsrc') != saddr:
-            continue
         # check the protocol
         protocol = 'rtp' if match.group('rtp_p_type') else 'rtcp'
         entry = match.groupdict()
         # massage values
         entry['frame_number'] = int(entry['frame_number'])
         entry['frame_time_epoch'] = float(entry['frame_time_epoch'])
-        entry['iplen'] = int(entry['iplen'])
+        entry['frame_time_relative'] = float(entry['frame_time_relative'])
+        # entry['ip_src'] = entry['ip_src']
+        ip_src = entry['ip_src']
+        entry['ip_len'] = int(entry['ip_len'])
         if protocol == 'rtp':
             entry['rtp_p_type'] = int(entry['rtp_p_type'])
+            rtp_p_type = entry['rtp_p_type']
             del entry['rtcp_pt']
             entry['rtp_ssrc'] = int(entry['rtp_ssrc'], 16)
+            rtp_ssrc = '%08x' % entry['rtp_ssrc']
             entry['rtp_seq'] = int(entry['rtp_seq'])
             entry['rtp_timestamp'] = int(entry['rtp_timestamp'])
             entry['rtp_marker'] = int(entry['rtp_marker'])
+            if ip_src not in parsed_rtp_list:
+                parsed_rtp_list[ip_src] = {}
+            if rtp_ssrc not in parsed_rtp_list[ip_src]:
+                parsed_rtp_list[ip_src][rtp_ssrc] = {}
+            if rtp_p_type not in parsed_rtp_list[ip_src][rtp_ssrc]:
+                parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type] = []
+            parsed_rtp_list[ip_src][rtp_ssrc][rtp_p_type].append(entry)
         elif protocol == 'rtcp':
             del entry['rtp_p_type']
             entry['rtcp_pt'] = int(entry['rtcp_pt'])
@@ -359,8 +429,10 @@ def parse_rtp_data(out, saddr, options):
             del entry['rtp_seq']
             del entry['rtp_timestamp']
             del entry['rtp_marker']
-        output[protocol].append(entry)
-    return output
+            if ip_src not in parsed_rtcp_list:
+                parsed_rtcp_list[ip_src] = []
+            parsed_rtcp_list[ip_src].append(entry)
+    return parsed_rtp_list, parsed_rtcp_list
 
 
 def get_options(argv):
@@ -383,6 +455,25 @@ def get_options(argv):
     parser.add_argument('-D', '--dry-run', action='store_true',
                         dest='dry_run', default=default_values['dry_run'],
                         help='Dry run',)
+    parser.add_argument('-a', '--analysis', action='store', type=str,
+                        dest='analysis_type',
+                        default=default_values['analysis_type'],
+                        choices=ANALYSIS_TYPES,
+                        metavar='ANALYSIS_TYPE',
+                        help='analysis type %r' % ANALYSIS_TYPES,)
+    parser.add_argument('--audio-jitter', action='store_const',
+                        dest='analysis_type', const='audio-jitter',
+                        metavar='ANALYSIS_TYPE',
+                        help='analysis type: audio-jitter',)
+    parser.add_argument('--audio-ploss', action='store_const',
+                        dest='analysis_type', const='audio-ploss',
+                        metavar='ANALYSIS_TYPE',
+                        help='analysis type: audio-ploss',)
+    parser.add_argument('--filter', action='store', type=str,
+                        dest='filter',
+                        default=default_values['filter'],
+                        metavar='FILTER',
+                        help='filter',)
     parser.add_argument('infile', type=str,
                         default=default_values['infile'],
                         metavar='input-file',
