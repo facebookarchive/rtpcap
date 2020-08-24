58dd619564fbc879373d5304ed4d74b071387287
Chema Gonzalez <chemag@fb.com>
2020-08-24 13:59:35 -0700

    rtpcap: add basic unittest
    Summary: Simple unittest, focused so far on the ploss/porder calculation.
    
    Test Plan:
    This is a unittest:
    
    ```
    $ ./rtpcap_unittest.py
    .
    ----------------------------------------------------------------------
    Ran 1 test in 0.000s
    
    OK
    ```

---
+++ rtpcap_unittest.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+"""rtpcap_unittest.py: simple unittest.
+
+
+"""
+
+import importlib
+import unittest
+
+rtpcap = importlib.import_module('rtpcap')
+
+
+getPacketLossAndOutOfOrderTestCases = [
+    {
+        'name': 'basic',
+        'rtp_seq_prev': 1,
+        'rtp_seq_list': [2, 3],
+        'ploss': 0,
+        'porder': 0,
+        'rtp_seq_max': 3,
+    },
+    {
+        'name': 'simple ploss 1',
+        'rtp_seq_prev': 1,
+        'rtp_seq_list': [2, 4],
+        'ploss': 1,
+        'porder': 0,
+        'rtp_seq_max': 4,
+    },
+    {
+        'name': 'simple porder 1',
+        'rtp_seq_prev': 1,
+        'rtp_seq_list': [2, 4, 3],
+        'ploss': 0,
+        'porder': 1,
+        'rtp_seq_max': 4,
+    },
+    {
+        'name': 'porder and ploss',
+        'rtp_seq_prev': 1,
+        'rtp_seq_list': [2, 4, 3, 6],
+        'ploss': 1,
+        'porder': 1,
+        'rtp_seq_max': 6,
+    },
+]
+
+
+class MyTest(unittest.TestCase):
+
+    def testGetPacketLossAndOutOfOrder(self):
+        """Simplest get_packets_loss_and_out_of_order test."""
+        for test_case in getPacketLossAndOutOfOrderTestCases:
+            ploss, porder, rtp_seq_max = (
+                rtpcap.get_packets_loss_and_out_of_order(
+                    test_case['rtp_seq_prev'],
+                    test_case['rtp_seq_list']))
+            msg = 'unittest failed: %s' % test_case['name']
+            self.assertEqual(test_case['ploss'], ploss, msg=msg)
+            self.assertEqual(test_case['porder'], porder, msg=msg)
+            self.assertEqual(test_case['rtp_seq_max'], rtp_seq_max, msg=msg)
+
+
+if __name__ == '__main__':
+    unittest.main()
